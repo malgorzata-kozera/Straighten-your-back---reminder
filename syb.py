2196093679da338fb177c862d767724674551434
Tsunade <malgorzata.kozera5@gmail.com>
2019-03-15 10:29:02 +0100

    added comments

--- syb.py
+++ syb.py
@@ -11,8 +11,8 @@ class PlayVoice:
     def __init__(self, master):
 
         self.master = master
-        self.selected_time = 900
-        self.master.protocol("WM_DELETE_WINDOW", self.close)
+        self.selected_time = 900  # default value of the alarm frequency
+        self.master.protocol("WM_DELETE_WINDOW", self.close)  # closing by 'x'
 
         self.select_button = Button(self.master, text='Select', width='10', command=self.select_time)
         self.select_button.grid(row=2, column=3)
@@ -35,7 +35,7 @@ class PlayVoice:
         self.l2 = Label(self.master, textvariable=self.label_text)
         self.l2.grid(row=10, column=1, columnspan=2)
 
-        self.timelist = Listbox(self.master, width='35', height='7')
+        self.timelist = Listbox(self.master, width='35', height='7')  # list with the frequency of the alarm to choose
 
         self.timelist.grid(row=2, column=0, rowspan=6, columnspan=2)
 
@@ -47,10 +47,9 @@ class PlayVoice:
 
         self.timelist.bind('<<ListboxSelect>>', self.get_selected_row)
 
-        self.playing = True
 
     def start_voice(self):
-
+        """method to start to play alarm. It calls a function play.voice() and set the label on empty """
         self.playing = True
         self.play_voice()
         self.label_text.set('')
@@ -59,7 +58,7 @@ class PlayVoice:
 
         def run():
 
-            self.sound = AudioSegment.from_file("voice.m4a")
+            self.sound = AudioSegment.from_file("voice.m4a")  # read audio file
             while self.playing:
                 play(self.sound)  # play sound
                 time.sleep(self.selected_time)
@@ -67,7 +66,7 @@ class PlayVoice:
                 if self.playing == False:
                     break
 
-        thread = threading.Thread(target=run)
+        thread = threading.Thread(target=run)  # it allows to run a multiple functions in the same time
         thread.daemon = True
         thread.start()
         self.start_button.config(state='disabled')
@@ -82,6 +81,7 @@ class PlayVoice:
         self.label_text.set('')
 
     def close_my_app(self):
+        """method to close app by pressing 'quit button' """
 
         self.exit = messagebox.askokcancel("Exit", "Are you sure you want to quit?")
         if self.exit:
@@ -93,6 +93,7 @@ class PlayVoice:
             pass
 
     def select_time(self):
+        """method to select a different time of alarm """
         self.start_button.config(state='active')
         self.label_text.set("Time has been selected, press Start to continue")
 
@@ -104,6 +105,7 @@ class PlayVoice:
         print(self.selected_time)
 
     def close(self):
+        """method to close app by pressing 'x' """
         self.selected_time = 0
         self.stop_voice()
         self.master.destroy()
